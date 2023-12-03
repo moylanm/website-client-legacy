9471423d4ea836cf557decfbcd097f5d484492d3
Myles Moylan <moylanm@fastmail.com>
2023-12-03 11:02:34 -0800

    Move gui instantiation logic to runner file

---
+++ cmd/gui/runner.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+)
+
+func (app *application) run() {
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Publish", app.publishForm()),
+		container.NewTabItem("Edit", app.editList()),
+	)
+
+	app.window.Resize(fyne.NewSize(750,500))
+	app.window.RequestFocus()
+	app.window.SetContent(tabs)
+	app.window.ShowAndRun()
+}
