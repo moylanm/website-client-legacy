ec4bb9f199e555876441efe9fe8e1324f7457f3b
Myles Moylan <moylanm@fastmail.com>
2023-12-03 14:28:26 -0800

    Implement "Publish" button validation handling

--- cmd/gui/widgets.go
+++ cmd/gui/widgets.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/widget"
+	"mylesmoylan.net/internal/validator"
 )
 
 
@@ -29,7 +30,14 @@ func (app *application) excerptForm() *widget.Form {
 		},
 		SubmitText: "Publish",
 		OnSubmit: func() {
+			excerpt := marshalExcerptForm(authorField.Text, workField.Text, tagsField.Text, bodyField.Text)
+			v := validator.New()
 
+			if validateExcerpt(v, excerpt); !v.Valid() {
+				app.showPopUp(v.ErrorsToString())
+			} else {
+				// publish
+			}
 		},
 	}
 }
