1207bb5a443f2f7e41232e2182bd1c7142f68dab
Myles Moylan <moylanm@fastmail.com>
2023-12-05 15:18:22 -0800

    Implement helper methods for editing excerpts

--- cmd/gui/net.go
+++ cmd/gui/net.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
 )
 
-func (app *application) publish(excerpt *Excerpt) string {
+func (app *application) publishExcerpt(excerpt *Excerpt) string {
 	js, err := json.Marshal(excerpt)
 	if err != nil {
 		return errorMessage("marshaling error", err)
@@ -15,7 +16,71 @@ func (app *application) publish(excerpt *Excerpt) string {
 
 	req, err := http.NewRequest(
 		http.MethodPost,
-		app.config.url,
+		app.config.publishUrl,
+		strings.NewReader(string(js)),
+	)
+	if err != nil {
+		return errorMessage("request creation error", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(app.config.admin.username, app.config.admin.password)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return errorMessage("request send error", err)
+	}
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+
+	return string(body)
+}
+
+func (app *application) listExcerpts() []Excerpt {
+	req, err := http.NewRequest(
+		http.MethodGet,
+		app.config.listUrl,
+		nil,
+	)
+	if err != nil {
+		fmt.Println(errorMessage("request creation error", err))
+		return []Excerpt{}
+	}
+
+	req.SetBasicAuth(app.config.admin.username, app.config.admin.password)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(errorMessage("request send error", err))
+		return []Excerpt{}
+	}
+	defer res.Body.Close()
+
+	var excerpts map[string][]Excerpt
+
+	dec := json.NewDecoder(res.Body)
+
+	err = dec.Decode(&excerpts)
+	if err != nil {
+		fmt.Println(errorMessage("json decoding error", err))
+		return []Excerpt{}
+	}
+
+	return excerpts["excerpts"]
+}
+
+func (app *application) updateExcerpt(id int64, excerpt *Excerpt) string {
+	js, err := json.Marshal(excerpt)
+	if err != nil {
+		return errorMessage("marshaling error", err)
+	}
+
+	req, err := http.NewRequest(
+		http.MethodPatch,
+		fmt.Sprintf("%s/%d", app.config.publishUrl, id),
 		strings.NewReader(string(js)),
 	)
 	if err != nil {
