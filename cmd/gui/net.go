ec33aef133c3763d33f9d90b3829793dac23fe22
Myles Moylan <moylanm@fastmail.com>
2023-12-07 15:30:23 -0800

    Check status code instead of message for server errors

--- cmd/gui/net.go
+++ cmd/gui/net.go
@@ -10,50 +10,10 @@ import (
 	"strings"
 )
 
-var messageRX = regexp.MustCompile(`"([\w\s]*)"\s*:\s*"([\w\s]*)"`)
+var messageRX = regexp.MustCompile(`:\s*"([\w\s]*)"`)
 
-func parseMessage(body []byte) (string, string) {
-	message := messageRX.FindStringSubmatch(string(body))
-
-	return message[1], message[2]
-}
-
-func (app *application) publishExcerpt(excerpt *Excerpt) (string, error) {
-	js, err := json.Marshal(excerpt)
-	if err != nil {
-		return "", err
-	}
-
-	req, err := http.NewRequest(
-		http.MethodPost,
-		app.config.publishUrl,
-		strings.NewReader(string(js)),
-	)
-	if err != nil {
-		return "", err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(app.config.admin.username, app.config.admin.password)
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-
-	message, desc := parseMessage(body)
-	if desc != "error" {
-		return "", errors.New(message)
-	}
-
-	return message, nil
+func parseMessage(body []byte) string {
+	return messageRX.FindStringSubmatch(string(body))[1]
 }
 
 func (app *application) listExcerpts() ([]Excerpt, error) {
@@ -87,6 +47,45 @@ func (app *application) listExcerpts() ([]Excerpt, error) {
 	return excerpts["excerpts"], nil
 }
 
+func (app *application) publishExcerpt(excerpt *Excerpt) (string, error) {
+	js, err := json.Marshal(excerpt)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		app.config.publishUrl,
+		strings.NewReader(string(js)),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(app.config.admin.username, app.config.admin.password)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	message := parseMessage(body)
+
+	if res.StatusCode != http.StatusCreated {
+		return "", errors.New(message)
+	}
+
+	return message, nil
+}
+
 func (app *application) updateExcerpt(excerpt Excerpt) (string, error) {
 	js, err := json.Marshal(excerpt)
 	if err != nil {
@@ -117,8 +116,9 @@ func (app *application) updateExcerpt(excerpt Excerpt) (string, error) {
 		return "", err
 	}
 
-	message, desc := parseMessage(body)
-	if desc != "error" {
+	message := parseMessage(body)
+
+	if res.StatusCode != http.StatusOK {
 		return "", errors.New(message)
 	}
 
@@ -149,8 +149,9 @@ func (app *application) deleteExcerpt(id int64) (string, error) {
 		return "", err
 	}
 
-	message, desc := parseMessage(body)
-	if desc != "error" {
+	message := parseMessage(body)
+
+	if res.StatusCode != http.StatusOK {
 		return "", errors.New(message)
 	}
 
