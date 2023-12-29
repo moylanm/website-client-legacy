672f9c99513d789d6734a7b0e7c87aef78df5bf7
Myles Moylan <moylanm@fastmail.com>
2023-12-29 11:35:21 -0800

    Set up edit tab list

--- v2/main.py
+++ v2/main.py
@@ -1,17 +1,27 @@
 # -.- coding: utf-8 -.-
-import sys, os
+import sys
 
 from net_handler import NetHandler
-from dotenv import load_dotenv
-from PySide6.QtCore import QRect
-from PySide6.QtWidgets import QApplication, QGroupBox, QLabel, QLineEdit, QMainWindow, QPushButton, QTabWidget, QTextEdit, QWidget
+from PySide6.QtCore import QRect, Qt
+from PySide6.QtWidgets import (
+    QApplication,
+    QHeaderView,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QTabWidget,
+    QTableWidget,
+    QTextEdit,
+    QWidget
+)
 
 class Main(QMainWindow):
 
-    def __init__(self, username, password, parent=None):
+    def __init__(self, parent=None):
         super().__init__(parent)
 
-        net_handler = NetHandler(username, password)
+        net_handler = NetHandler()
 
         self.setWindowTitle(u"Website Client")
         self.resize(800, 600)
@@ -38,28 +48,26 @@ class PublishTabView:
     def __init__(self, net_handler, parent):
         self.net_handler = net_handler
 
-        self.group_box = QGroupBox(parent)
-        self.group_box.resize(800, 560)
-        self.author_label = QLabel(self.group_box)
+        self.author_label = QLabel(parent)
         self.author_label.setText(u"Author :")
         self.author_label.setGeometry(QRect(15, 10, 60, 30))
-        self.work_label = QLabel(self.group_box)
+        self.work_label = QLabel(parent)
         self.work_label.setText(u"Work :")
         self.work_label.setGeometry(QRect(25, 50, 50, 30))
-        self.body_label = QLabel(self.group_box)
+        self.body_label = QLabel(parent)
         self.body_label.setText(u"Body :")
         self.body_label.setGeometry(QRect(25, 88, 60, 30))
-        self.author_field = QLineEdit(self.group_box)
+        self.author_field = QLineEdit(parent)
         self.author_field.setGeometry(QRect(70, 10, 330, 30))
-        self.work_field = QLineEdit(self.group_box)
+        self.work_field = QLineEdit(parent)
         self.work_field.setGeometry(QRect(70, 50, 330, 30))
-        self.body_field = QTextEdit(self.group_box)
+        self.body_field = QTextEdit(parent)
         self.body_field.setGeometry(QRect(70, 90, 690, 370))
-        self.publish_button = QPushButton(self.group_box)
+        self.publish_button = QPushButton(parent)
         self.publish_button.setText(u"Publish")
         self.publish_button.setGeometry(QRect(640, 465, 120, 40))
         self.publish_button.clicked.connect(self.publish)
-        self.clear_button = QPushButton(self.group_box)
+        self.clear_button = QPushButton(parent)
         self.clear_button.setText(u"Clear")
         self.clear_button.setGeometry(QRect(510, 465, 120, 40))
         self.clear_button.clicked.connect(self.clear)
@@ -82,18 +90,29 @@ class EditTabView:
 
     def __init__(self, net_handler, parent):
         self.net_handler = net_handler
+        self.excerpts = net_handler.list_excerpts()
 
-        self.group_box = QGroupBox(parent)
-        self.group_box.resize(800, 560)
-        for excerpt in self.net_handler.list_excerpts():
+        self.table = QTableWidget(len(self.excerpts), 1, parent)
+        self.table.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+        self.table.horizontalHeader().hide()
+        self.table.verticalHeader().hide()
+        self.table.resize(800, 560)
+
+        for row, excerpt in enumerate(self.excerpts):
+            button = QPushButton(f"{excerpt}")
+            button.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+            button.clicked.connect(self.on_clicked(excerpt))
+            self.table.setCellWidget(row, 0, button)
+    
+    def on_clicked(self, excerpt):
+        def new_window():
             print(excerpt)
-
+        return new_window
 
 if __name__ == "__main__":
-    load_dotenv()
-
     app = QApplication(sys.argv)
-    main = Main(os.getenv("USERNAME"), os.getenv("PASSWORD"))
+    main = Main()
 
     main.show()
     sys.exit(app.exec())
