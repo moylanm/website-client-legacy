e4f902d14b93bc7a8c9bb8ab5c43528ec8c67671
Myles Moylan <moylanm@fastmail.com>
2024-01-31 11:35:39 -0800

    Pattern matching

--- v2/helpers.py
+++ v2/helpers.py
@@ -2,14 +2,15 @@
 from PySide6.QtWidgets import QMessageBox
 
 def parse_response(response_data: dict) -> str:
-    if "message" in response_data:
-        return response_data["message"]
-    elif "error" in response_data:
-        return response_data["error"]
-    elif "errors" in response_data:
-        return "\n".join([f"{field}: {message}" for field, message in response_data["errors"].items()])
-    else:
-        return "Unknown response format"
+    match response_data:
+        case {"message": message}:
+            return message
+        case {"error": error}:
+            return error
+        case {"errors": errors} if isinstance(errors, dict):
+            return "\n".join([f"{field}: {message}" for field, message in errors.items()])
+        case _:
+            return "Unknown response format"
 
 def dialog_box(text: str) -> QMessageBox:
     db = QMessageBox()
