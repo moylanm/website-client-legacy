48ff21be1d24332253eaa504125cf2192c2bc43f
Myles Moylan <moylanm@fastmail.com>
2024-01-30 18:00:11 -0800

    Rename parse_response paramter

--- v2/helpers.py
+++ v2/helpers.py
@@ -1,17 +1,17 @@
 # -.- coding: utf-8 -.-
 from PySide6.QtWidgets import QMessageBox
 
-def parse_response(res: dict) -> str:
+def parse_response_dataponse(response_data: dict) -> str:
     try:
-        if "message" in res:
-            return res["message"]
-        elif "error" in res:
-            return res["error"]
-        elif "errors" in res:
-            errors = res["errors"]
+        if "message" in response_data:
+            return response_data["message"]
+        elif "error" in response_data:
+            return response_data["error"]
+        elif "errors" in response_data:
+            errors = response_data["errors"]
             return "\n".join([f"{field}: {error}" for field, error in errors.items()])
         else:
-            return "Unknown response format"
+            return "Unknown response_dataponse format"
     except Exception as e:
         return f"General error: {e}"
 
