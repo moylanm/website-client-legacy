79bd48a455e9cdbdc45b8b7a6766ed2a67b1d44c
Myles Moylan <moylanm@fastmail.com>
2024-01-30 18:06:02 -0800

    Remove try/catch block in parse_response

--- v2/helpers.py
+++ v2/helpers.py
@@ -2,18 +2,14 @@
 from PySide6.QtWidgets import QMessageBox
 
 def parse_response(response_data: dict) -> str:
-    try:
-        if "message" in response_data:
-            return response_data["message"]
-        elif "error" in response_data:
-            return response_data["error"]
-        elif "errors" in response_data:
-            errors = response_data["errors"]
-            return "\n".join([f"{field}: {error}" for field, error in errors.items()])
-        else:
-            return "Unknown response_dataponse format"
-    except Exception as e:
-        return f"General error: {e}"
+    if "message" in response_data:
+        return response_data["message"]
+    elif "error" in response_data:
+        return response_data["error"]
+    elif "errors" in response_data:
+        return "\n".join([f"{field}: {message}" for field, message in response_data["errors"].items()])
+    else:
+        return "Unknown response format"
 
 def dialog_box(text: str) -> QMessageBox:
     db = QMessageBox()
